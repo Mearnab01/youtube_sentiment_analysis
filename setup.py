f1bcedec384e3078641c6756696f2fb60dae0fa7
Arnab <Narnabnath919@gmail.com>
2026-07-05 14:40:37 +0530

    chore: remove frontend files and configurations for YouTube sentiment analysis extension

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ setup(
     name = 'youtube_sentiment',
     version= '0.0.1',
     author= 'Arnab',
-    author_email= 'your_email@example.com',
-    # This is the secret sauce for your 'src' folder:
     package_dir={"": "src"},
     packages=find_packages(where="src"),
 )
